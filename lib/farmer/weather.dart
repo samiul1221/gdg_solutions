a7a717daabe99deaa51eb07c0235b3d47deb483a
CoolQMan <suyashkumar123456@gmail.com>
2025-03-26 13:25:13 +0530

    Added icon and changed name

--- lib/farmer/weather.dart
+++ lib/farmer/weather.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -40,7 +41,7 @@ class _WeatherState extends State<Weather> {
       if (status.isGranted) {
         // Get current position
         Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high,
+          locationSettings: LocationSettings(accuracy: LocationAccuracy.best),
         );
 
         setState(() {
